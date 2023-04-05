e7928b58a6f611634550c80581b04bbc05c881af
HoangVyThong <hoangvythong1@gmail.com>
2023-04-05 17:29:03 +0700

    done_creatLayoutAdmin & update AdminController

--- BTL_Nhom7_N05/BTL_Nhom7_N05/Models/GioHang.cs
+++ BTL_Nhom7_N05/BTL_Nhom7_N05/Models/GioHang.cs
@@ -7,5 +7,36 @@ namespace BTL_Nhom7_N05.Models
 {
     public class GioHang
     {
+        BTL_WebEntities1 db = new BTL_WebEntities1();
+        public string MaThietBi { get; set; }
+        public string TenThietBi { get; set; }
+        public string Anh { get; set; }
+        public double DonGia { get; set; }
+        public int SoLuong { get; set; }
+        public int Max { get; set; }
+        public int ThanhTien1 { get; set; }
+        public int ThanhTien3 { get; set; }
+        public double ThanhTien
+        {
+            get { return DonGia * SoLuong; }
+            set { }
+        }
+        public GioHang(string MaThietBi)
+        {
+            this.MaThietBi = MaThietBi;
+            ThietBiYTe sanpham = db.ThietBiYTes.Single(n => n.MaThietBi == MaThietBi);
+            TenThietBi = sanpham.TenThietBi;
+            Anh = sanpham.Anh;
+            DonGia = (double)sanpham.GiaBan;
+            if ((int)sanpham.SoLuong == 0)
+            {
+                SoLuong = 0;
+            }
+            else
+            {
+                SoLuong = 1;
+            }
+            Max = (int)sanpham.SoLuong;
+        }
     }
 }
